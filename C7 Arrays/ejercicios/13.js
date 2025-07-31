43bafcb0b1a11212b81acc39a86362fe5c4c89e5
Stefano Masotti <stefanovmasotti@gmail.com>
2025-07-31 20:57:43 -0300

    C7 100%

--- C7 Arrays/ejercicios/13.js
+++ C7 Arrays/ejercicios/13.js
@@ -2,7 +2,7 @@ function filtrarNumerosPares(array) {
   // Devuelve un arreglo solo con los números pares presentes en el array.
   // Tu código:
   let newArray = [];
-  for (let i = 0; i < array.length-1; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (array[i]% 2 == 0) {
       newArray.push(array[i])
     }
--- C7 Arrays/ejercicios/33.js
+++ C7 Arrays/ejercicios/33.js
@@ -7,6 +7,27 @@ function combine(str1, str2, str3) {
   // combine("abc", "12345", "") == "a1b2c345"
   // combine("abc", "12345", "67") == "a16b27c345"
   // Tu código:
+  let suma = 0;
+  let newstr1 = []
+  let newstr2 = []
+  let newstr3 = []
+  let stringCompleto = [];
+  if (str1.length !== 0){
+    newstr1 = str1.split("");
+  }
+  if (str2.length !== 0){
+    newstr2 = str2.split("");
+  }
+  if (str3.length !== 0){
+    newstr3 = str3.split("");
+  }
+  suma = str1.length + str2.length + str3.length;
+  for (let i = 0; i < suma; i++) {
+    stringCompleto.push(str1[i]);
+    stringCompleto.push(str2[i]);
+    stringCompleto.push(str3[i]);
+  }
+  return stringCompleto.join("");
 }
 
 module.exports = combine;
